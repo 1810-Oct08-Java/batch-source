1439561dc25991f769aa28651e3d99d3308c1c56
Pantherhawk <alexrosenthalster@gmail.com>
2018-11-08 11:39:28 -0500

    2.3/ woah, learned how to exec multi-row insertions by adding 'from dual' clause

--- exercise.sql
+++ exercise.sql
@@ -12,3 +12,16 @@ SELECT * FROM ALBUM ORDER BY TITLE;
 
 SELECT * FROM CUSTOMER ORDER BY CITY ASC;
 
+----2.3 insert rows
+INSERT INTO GENRE VALUES 
+  (26, 'Post-Rock');
+INSERT INTO GENRE VALUES
+  (27, 'mATh rOcK');
+INSERT INTO EMPLOYEE VALUES
+  (16 + 1, 'Bob', 'Builder', 'IT Staff', 2, CURRENT_DATE, CURRENT_DATE, '42 Wallaby Ln','Calgary', 'AB', 'Canada', 'T2P 5G3', null, null, null);
+INSERT INTO EMPLOYEE VALUES
+  (19, 'Mr.', 'Potato', 'IT Staff', 2, CURRENT_DATE, CURRENT_DATE, '42 Wallaby Ln','Calgary', 'AB', 'Canada', 'T2P 5G3', null, null, null);
+
+INSERT INTO CUSTOMER (CUSTOMERID, FIRSTNAME, LASTNAME, COMPANY, ADDRESS, CITY, STATE, COUNTRY, POSTALCODE, PHONE, FAX, EMAIL, SUPPORTREPID)
+  SELECT 60, 'Rick', 'Sanchez', null, null, null, null, null, null, null, null, 'rick@ricketyrick.edu', null from dual
+  UNION ALL SELECT 61, 'Morty', 'Lastname', null, null, null, null, null, null, null, null, 'idontgotoschool@hardknocks.gov', null from dual;
