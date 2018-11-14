6ab3272e036db6525090130291d5d07d40b6338e
sherrymei <sherrymei95@gmail.com>
2018-11-14 14:47:11 -0500

    added CustomerInvoice hibernate exercise

---
+++ CustomerInvoice/src/main/java/com/revature/models/Customer.java
@@ -0,0 +1,76 @@
+package com.revature.models;
+
+public class Customer {
+	
+	private Integer id;
+	private String name;
+	private String phoneNumber;
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(Integer id, String name, String phoneNumber) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+}
---
+++ CustomerInvoice/src/main/java/com/revature/models/Invoice.java
@@ -0,0 +1,87 @@
+package com.revature.models;
+
+import java.sql.Date;
+
+public class Invoice {
+	
+	private int id;
+	private Date invoiceDate;
+	private double amount;
+	private Customer customer;
+	public Invoice(int id, Date invoiceDate, double amount, Customer customer) {
+		super();
+		this.id = id;
+		this.invoiceDate = invoiceDate;
+		this.amount = amount;
+		this.customer = customer;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getInvoiceDate() {
+		return invoiceDate;
+	}
+	public void setInvoiceDate(Date invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((invoiceDate == null) ? 0 : invoiceDate.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Invoice other = (Invoice) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (id != other.id)
+			return false;
+		if (invoiceDate == null) {
+			if (other.invoiceDate != null)
+				return false;
+		} else if (!invoiceDate.equals(other.invoiceDate))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Invoice [id=" + id + ", invoiceDate=" + invoiceDate + ", amount=" + amount + ", customer=" + customer
+				+ "]";
+	}
+
+	
+	
+}
